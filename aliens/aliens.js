9ab05ef1413dc228f6df53e3a29d838daaa9d481
Student <idk123@gmail.com>
2016-10-31 08:45:01 -1000

    new file:   aliens.js

---
+++ aliens/aliens.js
@@ -0,0 +1,18 @@
+//Aliens assignment
+var blue = 4000;
+var green = 3000;
+var red = 3000;
+
+var alien = blue + green + red;
+
+if (alien == 10000) {
+  if (green > 5000) {
+    console.log('We Gonna die');
+  }
+  if (green + red > 8000); {
+    console.log('anything')
+  }
+  if (red + blue < 3000); {
+    console.log('GG')
+  }
+}
